3fddaeac511628b7f96cff23603d0aaa97050102
Oscar-CR <krz.oficial@gmail.com>
2024-04-04 12:38:21 -0600

    reemplazo de fav icon faltante

--- resources/views/layouts/cotizador.blade.php
+++ resources/views/layouts/cotizador.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset('img/cropped-HH-Global-favicon-3-32x32.webp') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('img/cropped-HH-Global-favicon-3-32x32.webp') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
